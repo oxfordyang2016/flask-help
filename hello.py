00944261109e5c49ed01601c18a8ba18fc5215a6
yangming <yang756260386@gmail.com>
2017-02-22 18:22:26 +0800

    hello.py

--- hello.py
+++ hello.py
@@ -10,8 +10,10 @@ from flask import Flask,redirect, url_for,request,make_response
 import json
 import random
 import flask_login
-
-
+import MySQLdb
+#db = MySQLdb.connect(host="192.168.0.237", user="root", passwd="503951",db="cydex")
+db = MySQLdb.connect(host="192.168.201.142", user="root", passwd="123456",db="user")
+cursor = db.cursor()
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -124,6 +126,7 @@ reference html login.html
 </html>
 
 '''
+#how to get html form data in python
 @app.route('/success/<name>')
 def success(name):
    return 'welcome %s' % name
@@ -651,14 +654,72 @@ def testcanvasandajax():
 def testgithub():
     return "test github success"
 
-#-----------------flask authentication and login-----------
+
+
+#-----------------flask authentication and login logic and connect database-----------
 @app.route('/login2017', methods=['GET','POST'])
 def login2017():
     return render_template("login2017.html")
 
 @app.route('/register', methods=['GET','POST'])
 def sigup2017():
+    
     return render_template("register.html")
+
+@app.route('/registeruser', methods=['GET','POST'])
+def registeruser():
+    '''
+    i think there need 2 logic ,when vweity success,you 
+    need to switch a page,else return register
+    '''
+   if request.method == 'POST':
+       #get user info
+       username = request.form['username']
+       password = request.form['password']
+       return redirect(url_for('success',name = username))
+       #you must note it will redirect to success view function
+   else:
+       return render_template("register.html")
+
+
+
+
+
+@app.route('/verifyuser', methods=['GET','POST'])
+def verifyuser():
+    '''
+    i think there need 2 logic ,when vweity success,you 
+    need to switch a page,else return register
+    '''
+   if request.method == 'POST':
+       clientusername = request.form['username']
+       clientpassword = request.form['password']
+       cursor.execute("SELECT * FROM "+"verifyuser"+"  WHERE "+'username'+" ="+"'"+str(clientusername)+"'"+" AND "+'password'+'='+"'"+str(clientpassword)+"'"
+       if getrow()[1]!=0:
+           return redirect(url_for('success',name = username))
+           #you must note it will redirect to success view function
+   else:
+       return render_template("register.html")
+
+
+#------------------------------get database table row info- connect mysql-----
+
+def getrow():
+    # commit your changes
+    db.commit()
+    tabledict={}
+    numrows = int(cursor.rowcount)
+    num_fields = len(cursor.description)
+    field_names = [i[0] for i in cursor.description]
+    for x in range(0,numrows):
+        row = cursor.fetchone()
+        #print(row)
+        tmpdict={}
+        for k in range(0,len(row)):
+            #print str(field_names[k])+"|---------------------------->"+str(row[k]) 
+            tmpdict[str(field_names[k])]=str(row[k])
+        tabledict[str(x)]=tmpdict
+    return [tabledict,numrows]
 
 
 
