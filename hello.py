e1c8ec7bb365b7e63aaba010cf5eb01342efe316
yangming <yang756260386@gmail.com>
2017-02-22 19:35:27 +0800

    allok

--- hello.py
+++ hello.py
@@ -677,6 +677,7 @@ def registeruser():
         clientusername = request.form['username']
         clientpassword = request.form['password']
         cursor.execute("INSERT INTO verifyuser (username,password) VALUES ( "+str(clientusername)+','+str(clientpassword)+" )")
+        db.commit()
         return render_template('login2017.html')
         #you must note it will redirect to success view function
     else:
